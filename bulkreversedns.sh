ee5410bc7cde01c3a0036e2547189d2ca8f0c235
Felipe Molina <felmoltor@gmail.com>
2013-09-27 10:24:43 +0200

    Add function to validate IP address before request anything to the DNS server

--- bulkreversedns.sh
+++ bulkreversedns.sh
@@ -3,8 +3,33 @@
 # Summary: This script tries to obtain the name of an IP using dig
 # Author: Felipe Molina (@felmoltor)
 
+function isValidIP()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+##########
+# CONFIG #
+##########
+
 IP_FILE=""
 DNS_SERVER="8.8.8.8"
+
+################
+# CHECK PARAMS #
+################
 
 if [[ -f $1 ]];then
     IP_FILE=$1
@@ -13,14 +38,23 @@ else
     exit 1
 fi
 
-if [[ $2 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];then
+isValidIP $2
+valid_ip=$?
+if [[ $valid_ip == 0 ]];then
     DNS_SERVER=$2
-else
+else 
     echo "Wrong IP address specification. Using $DNS_SERVER by default"
 fi
 
+########
+# MAIN #
+########
+
 for ip in `cat $IP_FILE`; do
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];then
+    isValidIP $ip
+    valid_ip=$?
+    if [[ $valid_ip == 0 ]];then
+        
         echo -e "$ip;$(dig -x $ip @$DNS_SERVER +short)"
     else
         echo "Error: $ip is not a valid IP address. Skipping..."
